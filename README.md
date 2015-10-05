c23eb0aaaf43dc4572c8a2c426c40408a650928c
Lukasz Plotnicki <lplotnicki@thoughtworks.com>
2015-10-05 10:54:21 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # websocket-test
 WebSocket test with Spring &amp; Tomcat and Stomp.js
+
+## Run
+You need **gradle** installed and configured. Then just run
+
+```gradle tomcatRun```
+
+The app is then running under *http://localhost:8080/websocket*
+
+To enable SSL do following:
+
+```keytool -genkey -alias websocet_test -keyalg RSA```
+
+This will create a .keystore file in your home dir. Link this to the
+**tomcatRunWar.keyStoreFile** property.
+
+The app is then running under *https://localhost:8443/websocket*
---
+++ src/main/java/com/websockettest/config/WebSocketMessageBrokerConfig.java
@@ -0,0 +1,74 @@
+package com.websockettest.config;
+
+import com.websockettest.web.HandshakeInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/greetings").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic", "/queue");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        messageConverters.add(webSocketConverter());
+        return true;
+    }
+
+    @Bean
+    MappingJackson2MessageConverter webSocketConverter() {
+        return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    HttpSessionHandshakeInterceptor handshakeInterceptor() {
+        return new HandshakeInterceptor();
+    }
+}
